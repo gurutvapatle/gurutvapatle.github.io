521197cd5263a69fe19d19e7b230256f84e68c54
Gurutva <gurutvapatle@gmail.com>
2025-09-16 14:30:04 +0530

    Add files via upload

---
+++ publications/2025/AD-GS_videos_files/navigation.js
@@ -0,0 +1,10 @@
+
+function w3_open() {
+    document.getElementById("nav_panel").style.display = "block";
+    document.getElementById("content_overlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("nav_panel").style.display = "none";
+    document.getElementById("content_overlay").style.display = "none";
+}
